4e839afa97b5a7f2c28618bb0fde035a7d413fbc
rmit-kent-go <s3955624@student.rmit.edu.au>
2023-09-12 20:35:44 +1000

    created user.java

--- DataAnalyticsHub/src/analytics/model/User.java
+++ DataAnalyticsHub/src/analytics/model/User.java
@@ -1,5 +1,24 @@
 package analytics.model;
 
 public class User {
+    // Declaration of User's Instance Variables (private)
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
 
+    /**
+     * Constructor User to initialize instance variables in the object
+     * 
+     * @param username  The user's username
+     * @param password  The user's password
+     * @param firstName The user's firstName
+     * @param lastName  The user's lastName
+     */
+    public User(String username, String password, String firstName, String lastName) {
+	this.username = username;
+	this.password = password;
+	this.firstName = firstName;
+	this.lastName = lastName;
+    }
 }
