55fbd2ba83fba28780ab70e1ae4e6b2b0de6a64e
rmit-kent-go <s3955624@student.rmit.edu.au>
2023-09-12 20:39:32 +1000

    added getter and setter methods

--- DataAnalyticsHub/src/analytics/model/User.java
+++ DataAnalyticsHub/src/analytics/model/User.java
@@ -21,4 +21,60 @@ public class User {
 	this.firstName = firstName;
 	this.lastName = lastName;
     }
+    
+    /**
+     * The getter method to access user's username
+     */
+    public String getUsername() {
+	return username;
+    }
+    
+    /**
+     * The setter method to set user's username
+     */
+    public void setUsername(String username) {
+	this.username = username;
+    }
+    
+    /**
+     * The getter method to access user's password
+     */
+    public String getPassword() {
+	return password;
+    }
+    
+    /**
+     * The setter method to set user's password
+     */
+    public void setPassword(String password) {
+	this.password = password;
+    }
+    
+    /**
+     * The getter method to access user's firstName
+     */
+    public String getFirstName() {
+	return firstName;
+    }
+    
+    /**
+     * The setter method to set user's firstName
+     */
+    public void setFirstName(String firstName) {
+	this.firstName = firstName;
+    }
+    
+    /**
+     * The getter method to access user's lastName
+     */
+    public String getLastName() {
+	return lastName;
+    }
+    
+    /**
+     * The setter method to set user's lastName
+     */
+    public void setLastName(String lastName) {
+	this.lastName = lastName;
+    }
 }
